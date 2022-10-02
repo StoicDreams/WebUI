0a32350f5b6c4c8d735e4c492f86fedf2dd8cfef
Erik Gassler <erikgassler@live.com>
2022-10-02 09:57:01 -0700

    CI/CD: Fix script to use Unix Rust installer

--- AzurePipelineBuild.ps1
+++ AzurePipelineBuild.ps1
@@ -1,12 +1,18 @@
 $rust_installer_url = 'https://win.rustup.rs/x86_64'
 
 # Download Rust installer
-$fileName = $env:TEMP + (Split-Path -Path $rust_installer_url -Leaf)
-Write-Host "Executable File Name: $fileName"
-Invoke-RestMethod -Uri $rust_installer_url -OutFile $fileName
+if ($false) {
+	# For Windows
+	$fileName = $env:TEMP + (Split-Path -Path $rust_installer_url -Leaf)
+	Write-Host "Executable File Name: $fileName"
+	Invoke-RestMethod -Uri $rust_installer_url -OutFile $fileName
 
-# Run Rust installer
-Invoke-Item $fileName
+	# Run Rust installer
+	Invoke-Item $fileName
+} else {
+	# For Unix (Linux / Mac)
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+}
 
 # Run Rust updater to assure installation succeeded.
 rustup update
