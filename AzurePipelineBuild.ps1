876e3566e53c1f03a69f7dd474a889287d9e3a84
Erik Gassler <erikgassler@live.com>
2022-10-02 09:20:22 -0700

    CI/CD: Add check for rust installation

--- AzurePipelineBuild.ps1
+++ AzurePipelineBuild.ps1
@@ -1,8 +1,12 @@
 $rust_installer_url = 'https://win.rustup.rs/x86_64'
 
+# Download Rust installer
 $fileName = $env:TEMP + (Split-Path -Path $rust_installer_url -Leaf)
 Write-Host "Executable File Name: $fileName"
-
 Invoke-RestMethod -Uri $rust_installer_url -OutFile $fileName
 
+# Run Rust installer
 Invoke-Item $fileName
+
+# Run Rust updater to assure installation succeeded.
+rustup update
