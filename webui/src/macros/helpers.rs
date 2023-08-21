7fc53a3adb02c5244487e1124d4416b7161fcc0a
Erik Gassler <erik@gassler.us>
2023-08-21 18:51:15 -0500

    add PartialEq to define_form macro

--- webui/src/macros/helpers.rs
+++ webui/src/macros/helpers.rs
@@ -3,7 +3,7 @@ use crate::prelude::*;
 #[macro_export]
 macro_rules! define_form {
     ($name:ident, { $($field:ident : $ty:ty),* }) => {
-        #[derive(Debug, Clone, Deserialize, Serialize, Default)]
+        #[derive(Debug, Clone, Deserialize, Serialize, Default, PartialEq)]
         pub struct $name {
             $(pub $field : $ty),*,
         }
