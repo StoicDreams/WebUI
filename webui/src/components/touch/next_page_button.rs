1745d354e6e33a8dc24490379275fd7aae06b1ff
Erik Gassler <erik@gassler.us>
2023-10-13 13:52:11 -0500

    sdauth polish

--- webui/src/components/touch/next_page_button.rs
+++ webui/src/components/touch/next_page_button.rs
@@ -4,10 +4,12 @@ use crate::prelude::*;
 pub struct NextPageProps {
     pub display: Option<String>,
     pub url: String,
+    #[prop_or(true)]
+    pub snap_bottom: bool,
 }
 
 #[function_component(NextPageButton)]
-pub fn next_page_button(_props: &NextPageProps) -> Html {
+pub fn next_page_button(props: &NextPageProps) -> Html {
     #[cfg(not(feature = "nextpagebutton"))]
     {
         return html! {};
@@ -16,17 +18,19 @@ pub fn next_page_button(_props: &NextPageProps) -> Html {
     {
         let contexts = use_context::<Contexts>().expect("Contexts not found");
         let app_config = contexts.config;
-        let display = match _props.display.to_owned() {
+        let display = match props.display.to_owned() {
             Some(display) => display,
-            None => match app_config.get_nav_from_path(&_props.url) {
+            None => match app_config.get_nav_from_path(&props.url) {
                 Some(nav) => nav.name,
                 None => "Not Found".to_string(),
             },
         };
-        let href = _props.url.to_owned();
+        let href = props.url.to_owned();
         html!(
             <>
-                <Paper class="flex-grow" />
+                if props.snap_bottom {
+                    <Paper class="flex-grow" />
+                }
                 <Paper class="d-flex flex-column align-center justify-center ma-5 pa-5">
                     <Link class="btn theme-info" {href}>
                         <span>{format!("Continue to {}", display)}</span>
--- webui/src/myfi/myfi_pages/myfi_sdauth.rs
+++ webui/src/myfi/myfi_pages/myfi_sdauth.rs
@@ -15,6 +15,20 @@ pub(crate) fn get_myfi_auth_token_key() -> String {
 #[function_component(RenderPage)]
 fn render_page() -> Html {
     let contexts = use_context::<Contexts>().expect("Contexts not found");
+    let auth_key = query_url("key");
+    if auth_key.is_none() {
+        return html! {
+            <>
+                <MyFiStorageConcent />
+                <Paper class="d-flex flex-column justify-left align-left">
+                    <Quote color={Theme::Success}>
+                        {"You "}
+                    </Quote>
+                </Paper>
+                <NextPageButton url="/" snap_bottom={false} />
+            </>
+        };
+    }
     let page_messages = use_state(|| {
         String::from(
             r#"
@@ -24,66 +38,63 @@ Validating account.
 "#,
         )
     });
+    let displayed_markdown = page_messages.deref().to_owned();
+    if !displayed_markdown.contains("Validating account.") {
+        return html! {
+            <>
+                <MyFiStorageConcent />
+                <Paper class="d-flex flex-column justify-left align-left">
+                    <MarkdownContent markdown={displayed_markdown} />
+                </Paper>
+                <NextPageButton url="/" snap_bottom={false} />
+            </>
+        };
+    }
     let pmthread = page_messages.to_owned();
-    if page_messages.contains("Validating account.") {
-        spawn_async!({
-            match query_url("key") {
-                Some(key) => {
-                    set_user_storage_data(get_myfi_auth_token_key(), key);
-                    let user_state = contexts.clone().user;
-                    match myfi_get_my_info(user_state).await {
-                        true => {
-                            pmthread.set(String::from(
-                                r#"
+    let auth_key = auth_key.to_owned();
+    spawn_async!({
+        match auth_key {
+            Some(key) => {
+                set_user_storage_data(get_myfi_auth_token_key(), key);
+                let user_state = contexts.clone().user;
+                match myfi_get_my_info(user_state).await {
+                    true => {
+                        pmthread.set(String::from(
+                            r#"
 ```quote "success"
 Sign-in to Stoic Dreams account successful.
 ```
-```paper
-[Continue to Home](/)
-```
 "#,
-                            ));
-                        }
-                        false => {
-                            pmthread.set(String::from(
-                                r#"
+                        ));
+                        nav_to!(contexts, "/sdauth");
+                    }
+                    false => {
+                        pmthread.set(String::from(
+                            r#"
 ```quote "danger"
 Sign-in to Stoic Dreams account failed. Key is invalid or may have expired.
 ```
 "#,
-                            ));
-                            set_user_storage_data(get_myfi_auth_token_key(), String::default());
-                        }
+                        ));
+                        set_user_storage_data(get_myfi_auth_token_key(), String::default());
                     }
                 }
-                None => {
-                    pmthread.set(String::from(
-                        r#"
+            }
+            None => {
+                pmthread.set(String::from(
+                    r#"
 ```quote "danger"
 Expected key was not found for account authentication.
 ```
 "#,
-                    ));
-                }
+                ));
             }
-        });
-    }
-    let displayed_markdown = page_messages.deref().to_owned();
-    if displayed_markdown.contains("Validating") {
-        return html! {
-            <>
-                <MyFiStorageConcent />
-                <Paper class="d-flex flex-grow flex-column justify-left align-left">
-                    <MarkdownContent markdown={displayed_markdown} />
-                </Paper>
-                <Paper />
-            </>
-        };
-    }
+        }
+    });
     html! {
         <>
             <MyFiStorageConcent />
-            <Paper class="d-flex flex-grow flex-column justify-left align-left">
+            <Paper class="d-flex flex-column justify-left align-left">
                 <MarkdownContent markdown={displayed_markdown} />
             </Paper>
         </>
