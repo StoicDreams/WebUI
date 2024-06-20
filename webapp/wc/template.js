3c3f84c66aa8fe3d10378859d13428fbbf061cd0
Erik Gassler <erik@gassler.us>
2024-06-20 14:03:03 -0500

    template fix

--- webapp/wc/template.js
+++ webapp/wc/template.js
@@ -1,16 +1,26 @@
-/* Template for web components. */
+/* Template for open web components. */
 "use strict"
 {
-    // Start open Template
+    function toCamel(property) {
+        return property.replace(/(-[A-Za-z0-9]{1})/g, a => { return a[1].toUpperCase(); });
+    }
     class OpenTemplate extends HTMLElement {
         constructor() {
             super();
             const t = this;
+            if (t.parentNode && t.parentNode.nodeName === 'P') {
+                let p = t.parentNode;
+                t.parentNode.parentNode.insertBefore(t, t.parentNode);
+                if (p.innerHTML.trim() === '') {
+                    p.remove();
+                }
+            }
         }
         static get observedAttributes() {
             return [];
         }
         attributeChangedCallback(property, oldValue, newValue) {
+            property = toCamel(property);
             if (oldValue === newValue) return;
             if (newValue === null || newValue === undefined) {
                 delete this[property];
@@ -22,9 +32,11 @@
         disconnectedCallback() { }
     }
     customElements.define('app-open-template', OpenTemplate);
-    // End Open Template
+}
 
-    // Start Shadow Template
+/* Template for open web components. */
+"use strict"
+{
     const template = document.createElement('template')
     template.setAttribute('shadowrootmode', true);
     template.innerHTML = `
@@ -35,10 +47,20 @@
 <slot></slot>
 <slot name="something"></slot>
 `;
+    function toCamel(property) {
+        return property.replace(/(-[A-Za-z0-9]{1})/g, a => { return a[1].toUpperCase(); });
+    }
     class ShadowTemplate extends HTMLElement {
         constructor() {
             super();
             const t = this;
+            if (t.parentNode && t.parentNode.nodeName === 'P') {
+                let p = t.parentNode;
+                t.parentNode.parentNode.insertBefore(t, t.parentNode);
+                if (p.innerHTML.trim() === '') {
+                    p.remove();
+                }
+            }
             const shadow = t.attachShadow({ mode: 'open' });
             t.template = template.content.cloneNode(true);
             shadow.appendChild(t.template);
@@ -47,6 +69,7 @@
             return [];
         }
         attributeChangedCallback(property, oldValue, newValue) {
+            property = toCamel(property);
             if (oldValue === newValue) return;
             if (newValue === null || newValue === undefined) {
                 delete this[property];
@@ -58,5 +81,4 @@
         disconnectedCallback() { }
     }
     customElements.define('app-shadow-template', ShadowTemplate);
-    // End Shadow Template
 }
