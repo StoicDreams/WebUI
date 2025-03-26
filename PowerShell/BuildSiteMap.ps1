6a5ddf31b3e0806e7c929f8020735eccaaaefc46
Erik Gassler <erik@gassler.us>
2025-03-26 08:11:26 -0500

    Add Google Analytics

--- PowerShell/BuildSiteMap.ps1
+++ PowerShell/BuildSiteMap.ps1
@@ -3,35 +3,58 @@ $domain = "https://webui.stoicdreams.com"
 
 $urls = [System.Collections.ArrayList]@();
 
-$appsettings = Get-Content "webapp/root_files/nav.json" -Raw
+$appsettings = Get-Content "webapp/root_files/nav.json" -Raw | ConvertFrom-Json
 
 if ($appsettings.Length -eq 0) {
-	throw "webapp/root_files/nav.json file not found";
+    throw "webapp/root_files/nav.json file not found";
 }
 
-$appsettings | Select-String -Pattern '(?<="url":[ ]*)("([^"]*)")' -AllMatches | ForEach-Object { $_.Matches } | ForEach-Object {
-    $item = @{url = $_.Value};
-    $urls.Add($item) | Out-Null
+function ProcessNode {
+    param (
+        [Parameter(Mandatory = $true)]
+        [PSObject]$Node
+    )
+
+    $role = 0;
+
+    if ($Node.role) {
+        $role = $Node.role
+    }
+
+    if ($role -gt 0) { return; }
+
+    if ($Node.url) {
+        $item = @{url = $Node.url }
+        $urls.Add($item) | Out-Null
+    }
+
+    # Check if the node has children
+    if ($Node.children) {
+        # Recursively process each child node
+        foreach ($child in $Node.children) {
+            ProcessNode -Node $child
+        }
+    }
 }
 
-function buildSitemap
-{
-    begin
-    {
+foreach ($node in $appsettings) {
+    ProcessNode -Node $node
+}
+
+function buildSitemap {
+    begin {
         '<?xml version="1.0" encoding="utf-8" ?>'
         '<urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">'
     }
 
-    process
-    {
+    process {
         '    <url>'
         "        <loc>$($domain)$($_.url)</loc>"
         "        <lastmod>{0:yyyy-MM-dd}</lastmod>" -f (Get-Date).Date
         '    </url>'
     }
 
-    end
-    {
+    end {
         "</urlset>"
     }
 }
