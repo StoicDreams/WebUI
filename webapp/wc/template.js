fe7de58f254a95da9b99a75fa18660312adab50a
Erik Gassler <erik@gassler.us>
2025-06-11 14:49:31 -0500

    template update

--- webapp/wc/template.js
+++ webapp/wc/template.js
@@ -1,32 +1,66 @@
 /* Template for Web UI components. */
 "use strict"
-webui.define("app-template", {
-    constructor: (t) => { },
-    attr: ['example'],
-    attrChanged: (t, property, value) => {
-        switch (property) {
-            case 'example':
-                break;
-        }
-    },
-    connected: function (t) { },
-    disconnected: function (t) { }
-});
-
+{
+    webui.define("app-template", {
+        watchVisibility: false,
+        isInput: false,
+        preload: '',
+        constructor: (t) => { },
+        flags: [],
+        attr: ['height', 'max-height'],
+        attrChanged: (t, property, value) => {
+            switch (property) {
+                case 'height':
+                    t.style.height = webui.pxIfNumber(value);
+                    break;
+                case 'maxHeight':
+                    t.style.maxHeight = webui.pxIfNumber(value);
+                    break;
+            }
+        },
+        connected: function (t) {
+            t.setupComponent();
+        },
+        disconnected: function (t) { },
+        reconnected: function (t) { },
+        setupComponent: function () {
+            const t = this;
+        },
+    });
+}
 /* Template for Web UI components. */
 "use strict"
-webui.define("app-shadow-template", {
-    constructor: (t) => { },
-    attr: ['example'],
-    attrChanged: (t, property, value) => {
-        switch (property) {
-            case 'example':
-                break;
-        }
-    },
-    connected: function (t) { },
-    disconnected: function (t) { },
-    shadowTemplate: `
+{
+    webui.define("app-shadow-template", {
+        linkCss: false,
+        watchVisibility: false,
+        isInput: false,
+        preload: '',
+        constructor: (t) => {
+            t._slotMain = t.template.querySelector('slot:not([name])');
+            t._slotSomething = t.template.querySelector('slot[name="something"]');
+        },
+        flags: [],
+        attr: ['height', 'max-height'],
+        attrChanged: (t, property, value) => {
+            switch (property) {
+                case 'height':
+                    t.style.height = webui.pxIfNumber(value);
+                    break;
+                case 'maxHeight':
+                    t.style.maxHeight = webui.pxIfNumber(value);
+                    break;
+            }
+        },
+        connected: function (t) {
+            t.setupComponent();
+        },
+        disconnected: function (t) { },
+        reconnected: function (t) { },
+        setupComponent: function () {
+            const t = this;
+        },
+        shadowTemplate: `
 <style type="text/css">
 :host {
 }
@@ -34,4 +68,5 @@ webui.define("app-shadow-template", {
 <slot></slot>
 <slot name="something"></slot>
 `
-});
+    });
+}
